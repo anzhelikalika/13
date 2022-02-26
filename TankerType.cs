e8003f23bbec50d9aa872a6b9e52b603de604f93
anzhelikalika <90254561+anzhelikalika@users.noreply.github.com>
2022-02-26 12:34:37 +0300

    Create TankerType.cs

---
+++ TankerType.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_task5_8
+{
+    public enum TankerType
+    {
+        HANDYMAX,
+        SEAWAYMAX,
+        PANAMAX,
+        AFRAMAX,
+        SUEZMAX,
+        MALACCAMAX
+    }
+}
